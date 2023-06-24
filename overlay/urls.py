3564e2da72c7f8bec3c5bff17b93e14b5e24dde3
Sukanth07 <sukanthkannansk@gmail.com>
2023-06-24 16:10:12 +0530

    face

--- overlay/urls.py
+++ overlay/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("overlay_jewellery/", views.overlay_jewellery, name="overlay_jewellery"),
-    path("overlay_earrings/", views.overlay_earrings, name="overlay_earrings")
+    path("overlay_earrings/", views.overlay_earrings, name="overlay_earrings"),
+    path("face_shape/", views.face_shape, name="face_shape")
 ]
